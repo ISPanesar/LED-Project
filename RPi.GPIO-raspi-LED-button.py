14e48e55813c5fcdb46ba969633d7ca6cb4079f8
ISPanesar <46325131+ISPanesar@users.noreply.github.com>
2019-01-02 16:44:35 +0000

    Add files via upload

--- RPi.GPIO-raspi-LED-button.py
+++ RPi.GPIO-raspi-LED-button.py
@@ -2,24 +2,21 @@ import os
 from time import sleep
 
 import RPi.GPIO as GPIO
-
+import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN)
 GPIO.setup(24, GPIO.IN)
 GPIO.setup(25, GPIO.IN)
-GPIO.setup(5, GPIO.OUT)
-GPIO.setup(6, GPIO.OUT)
+GPIO.setup(5, GPIO.OUT, initial=0)
+GPIO.setup(6, GPIO.OUT, initial=0)
 
 while True:
     if not GPIO.input(23):
         GPIO.output(5, 1)
-        sleep(1)
-        GPIO.output(5, 0)
 
     if not GPIO.input(24):
         GPIO.output(6, 1)
-        sleep(1)
-        GPIO.output(6, 0)
+
     if not GPIO.input(25):
         import sys
 
